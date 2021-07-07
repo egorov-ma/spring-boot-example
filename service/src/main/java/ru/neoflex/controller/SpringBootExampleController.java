010f1cdb31ccdfb1060195aed33fc95ae625d067
megorov <egorov-ma>
2021-07-07 15:26:01 +0300

    upd feature

--- service/src/main/java/ru/neoflex/controller/SpringBootExampleController.java
+++ service/src/main/java/ru/neoflex/controller/SpringBootExampleController.java
@@ -35,6 +35,8 @@ public class SpringBootExampleController {
 //        return Mono.just("test - getTest");
 //    }
 //
+
+
 //    @PostMapping("/test/{param}")
 //    private Flux<String> postTest(
 //            @PathVariable String input,
